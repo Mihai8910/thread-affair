efe6cd1575dd48bf46e19061b2f2256ed4d6ce85
Mihai8910 <101422568+Mihai8910@users.noreply.github.com>
2022-06-13 22:05:33 +0300

    thread Affair

---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Thread Affair</title>
+      </Head>
+
+<header className="header">Hello</header>
+
+<main className="main"></main>
+
+<footer className="footer"></footer>
+
+
+    </div>
+  )
+}
